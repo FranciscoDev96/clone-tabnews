3aecdb30c4333600a24f47645a4ac3fdf647f5c2
Francisco Sales de Lima Junior <salesf345@gmail.com>
2025-07-01 17:05:00 +0000

    Adiciona uma exclamação no final da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Dona cabexinha, te amo! Se vc me ama de volta, da uma risadinha.</h1>
+    return <h1>Dona cabexinha, te amo! Se vc me ama de volta, da uma risadinha!</h1>
 }
 
 export default Home;
